d0caaea9651197854d4240b14abc4f4a62a3b49e
marwamamlouk <marwamamlouk12@gmail.com>
2022-05-14 23:21:31 +0100

    LOGINCONTROLLER

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
 
 class LoginController extends Controller
 {
@@ -28,6 +30,14 @@ class LoginController extends Controller
      */
     protected $redirectTo = RouteServiceProvider::HOME;
 
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            $this->username() => 'required|string',
+            'password' => 'required|string',
+            'g-recaptcha-response' => 'required|captcha',
+        ]);
+    }
     /**
      * Create a new controller instance.
      *
